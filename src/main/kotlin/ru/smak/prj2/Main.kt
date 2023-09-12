5545c87f40d7acdb138081987234b0b7084550bb
Sergey Makletsov <smaklets@kpfu.ru>
2023-09-12 16:10:09 +0300

    Лекция от 12.09.23

--- src/main/kotlin/ru/smak/prj2/Main.kt
+++ src/main/kotlin/ru/smak/prj2/Main.kt
@@ -1,2 +1,16 @@
 package ru.smak.prj2
 
+fun main() {
+    val lst = listOf(1, 2, 3)
+    val mlst = mutableListOf<Int>()
+    mlst.add(123)
+    println(mlst.joinToString())
+    val map1 = mutableMapOf(1 to 1, 2 to 10, 3 to 100)
+    println(map1[3])
+    val hm = HashMap<Int, Double>()
+    hm.put(1, 3.5)
+    hm.put(2, 4.5)
+    hm.put(2, 7.5)
+
+    println(hm.toList().joinToString())
+}
--- src/main/kotlin/ru/smak/prj2/MyClass.kt
+++ src/main/kotlin/ru/smak/prj2/MyClass.kt
@@ -1,4 +1,51 @@
 package ru.smak.prj2
 
-class MyClass {
+import kotlin.random.Random
+
+class MyClass(var p: Int) {
+    var a: Int
+    val a2: Int
+        get() = a * a
+    var b: Int = 0
+        set(value) {
+            field = value * 2
+        }
+    var t: String = ""
+
+    init{
+        a = 0
+    }
+
+    constructor() : this(0)
+
+    override fun toString() = "a = $a; a2 = $a2; b = $b; p=$p"
+}
+
+var mc: MyClass? = null
+fun main() {
+
+    val br = Random.nextBoolean()
+    if (br) mc = MyClass(30)
+    println(mc)
+
+    println(mc?.run{
+        a = 100
+        b = 10
+        t = "Привет"
+        println(t.length)
+        a * b
+    })
+
+    val mc2 = MyClass().apply{
+        a = 100
+        b = 100
+        t = "Текст"
+    }
+
+    println(with(mc2){
+        a = 300
+        b = 764
+        a * b
+    })
+
 }
